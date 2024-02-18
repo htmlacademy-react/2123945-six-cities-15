8636adfe3d811750575ce2872ae645c8b82369a6
Alena Aleksenko <arslanbekova@gmail.com>
2024-02-18 16:52:52 +0100

    added const Setting={offerCount: 312}

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app';
+import App from './components/app/App';
+import { Setting } from './const.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offerCount={312} />
+    <App offerCount={Setting.offerCount} />
   </React.StrictMode>
 );
