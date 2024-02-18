8ebcd28e250f7bb0450a3bf76c85b63e8dd79c64
Alena Aleksenko <arslanbekova@gmail.com>
2024-02-18 16:21:38 +0100

    created <App/>, render <App/> in index.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import MainPage from '../pages/main-page/main-page';
+
+type MainPageScreenProps = {
+  offerCount: number;
+};
+
+function App({ offerCount }: MainPageScreenProps): JSX.Element {
+  return <MainPage offerCount={offerCount} />;
+}
+
+export default App;
